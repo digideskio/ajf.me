d8943c67e09215156d81a57135731f65d4002bf8
Andrea Faulds <ajf@ajf.me>
2013-11-08 17:56:53 +0000

    Stars! :3

---
+++ htdocs/demos/stars/stars.js
@@ -0,0 +1,66 @@
+(function () {
+    'use strict';
+    var secs;
+
+    secs = function () {
+        return new Date().getTime() / 1000;
+    };
+
+    window.onload = function () {
+        var canvas, ctx, stars, star, i, render;
+
+        canvas = document.createElement('canvas');
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerWidth;
+        canvas.id = 'stars';
+        canvas.style.position = 'fixed';
+        canvas.style.left = canvas.style.right = canvas.style.top = canvas.style.bottom = '0px';
+        document.body.appendChild(canvas);
+
+        ctx = canvas.getContext('2d');
+
+        stars = [];
+
+        render = function render() {
+            var starnum, starspeed, now, size;
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+            ctx.fillStyle = 'rgb(0, 0, 0)';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+            starnum = Math.floor(Math.sqrt(canvas.width * canvas.height) / 2);
+            starspeed = canvas.width / 8;
+
+            if (stars.length < starnum - 1) {
+                for (i = stars.length; i < starnum; i += 1) {
+                    star = {
+                        x: Math.random() * canvas.width,
+                        y: Math.random() * canvas.height,
+                        depth: Math.pow(Math.random(), 2),
+                        prev: secs()
+                    };
+                    stars.push(star);
+                }
+            } else if (stars.length > starnum - 1) {
+                stars.splice(starnum);
+            }
+
+            for (i = 0; i < stars.length; i += 1) {
+                now = secs();
+                stars[i].x -= stars[i].depth * starspeed * (now - stars[i].prev);
+                stars[i].prev = now;
+
+                while (stars[i].x < 0) {
+                    stars[i].x += canvas.width;
+                }
+
+                size = 3 * stars[i].depth;
+                ctx.fillStyle = 'white';
+                ctx.fillRect(stars[i].x - size / 2, stars[i].y - size / 2, size, size);
+            }
+
+            requestAnimationFrame(render);
+        };
+        render();
+    };
+}());
